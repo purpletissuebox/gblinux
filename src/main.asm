f52162f75fdcde25b1b380b49185a35a09d11987
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-02-26 22:25:42 -0500

    expand init function and test include

--- src/main.asm
+++ src/main.asm
@@ -1,10 +1,36 @@
-SECTION "HEADER", ROM0[$0100]
-nop
-jp entry
+INCLUDE "./src/common/macros.asm"
+INCLUDE "./src/common/ioregs.asm"
 
-ds $150-@, 0
+SECTION "HEADER", ROM0[$0100]
+entryPoint:
+	di
+	jp init
+
+ds $150-@ ;reserve space for logo, etc
 
 SECTION "INIT", ROM0
-entry:
-halt
-jr entry
+init::
+	ld sp, 0xD000
+	ld hl, oam_routine
+	ld de, 0xFF80
+	getsize oam_routine, c
+	.memcpy:
+		ldi a, [hl]
+		ld [de], a
+		inc de
+		dec c
+	jr nz, .memcpy
+	
+end:
+	halt
+	jr end
+
+SECTION "OAM DMA", ROM0
+oam_routine:
+	ld [IO_OAM_DMA], a
+	ld a, 0x28
+	.stall:
+		dec a
+		jr nz, .stall
+	ret
+	.end
