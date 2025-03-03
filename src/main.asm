74abc21a749da35269378a7e17552ca0f58d46e5
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-03-03 01:16:48 -0500

    move implementation to other files. add vectors and dummy main loop

--- src/main.asm
+++ src/main.asm
@@ -1,36 +1,76 @@
-INCLUDE "./src/common/macros.asm"
-INCLUDE "./src/common/ioregs.asm"
+INCLUDE "hwregs.inc"
 
-SECTION "HEADER", ROM0[$0100]
+SECTION "VECTORS", ROM0[0x0000]
+call_hl::
+	jp hl
+	ds 0x0008 - @
+memcpy::
+	ld a, [de]
+	inc de
+	ldi [hl], a
+	dec c
+	jr nz, memcpy
+	ret
+	ds 0x0010 - @
+strcpy::
+	ld a, [de]
+	inc de
+	and a
+	ret z
+	ldi [hl], a
+	jr strcpy
+	ds 0x0018 - @
+waitVRAM::
+	;routine takes 44 dots at most, ppu takes 167 dots at least.
+	;so worst cast you have 123 dots = 30 clocks of vram time after calling this
+	ldh a, [IO_LCD_STATUS]
+	and PPU_MODE
+	jr nz, waitVRAM
+	ret
+	ds 0x0020 - @
+memset::
+	ldi [hl], a
+	dec c
+	jr nz, memset
+	ret
+	ds 0x0028 - @
+rst_28::
+	ret
+	ds 0x0030 - @
+rst_30::
+	ret
+	ds 0x0038 - @
+rst_38::
+	jr rst_38
+	ds 0x0040 - @
+	
+int_vblank::
+	push af
+	ldh a, [redraw_screen]
+	and a
+	jp nz, VBLANK
+	pop af
+	ds 0x0048 - @
+int_lcd::
+	reti
+	ds 0x0050 - @
+int_timer::
+	reti
+	ds 0x0058 - @
+int_serial::
+	reti
+	ds 0x0060 - @
+int_joypad::
+	reti
+
+SECTION "HEADER", ROM0[0x0100]
 entryPoint:
-	di
+	nop
 	jp init
 
 ds $150-@ ;reserve space for logo, etc
 
-SECTION "INIT", ROM0
-init::
-	ld sp, 0xD000
-	ld hl, oam_routine
-	ld de, 0xFF80
-	getsize oam_routine, c
-	.memcpy:
-		ldi a, [hl]
-		ld [de], a
-		inc de
-		dec c
-	jr nz, .memcpy
-	
-end:
+SECTION "MAIN", ROM0
+MAIN::
 	halt
-	jr end
-
-SECTION "OAM DMA", ROM0
-oam_routine:
-	ld [IO_OAM_DMA], a
-	ld a, 0x28
-	.stall:
-		dec a
-		jr nz, .stall
-	ret
-	.end
+	jr MAIN
