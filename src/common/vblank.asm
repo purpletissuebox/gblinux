7460a53e8f5a8646201e0101f64fd52d610291d7
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-03-07 21:06:17 -0500

    treat redraw variable as a bitfield. palettes can be copied in hblank.

--- src/common/vblank.asm
+++ src/common/vblank.asm
@@ -4,6 +4,7 @@ DEF COLORS_PER_LOOP EQU 2
 SECTION "VBLANK_HANDLER", ROM0
 VBLANK::
 	push bc
+	push de
 	push hl
 	
 	ldh a, [ram_bank]
@@ -11,43 +12,54 @@ VBLANK::
 	ld a, BANK("GFX_VARS")
 	ldh [IO_WRAM_BANK], a
 	
-	call oamRoutine
-	
-	ld hl, shadow_scroll_y
-	ldi a, [hl]
-	ldh [IO_SCROLL_Y], a
-	ldi a, [hl]
-	ldh [IO_SCROLL_X], a
-	ldi a, [hl]
-	ldh [IO_WINDOW_Y], a
-	ldi a, [hl]
-	ldh [IO_WINDOW_X], a
-	
-	ld c, LOW(IO_CRAM_BKG_SELECT)
-	.loadColors:
-		ld b, 0x20/COLORS_PER_LOOP
-		ld a, CRAM_INCREMENT
-		ldh [c], a
-		inc c
-		.copy:
-			REPT COLORS_PER_LOOP*2
-			ldi a, [hl]
-			ldh [c], a
-			ENDR
-			dec b
-		jr nz, VBLANK.copy
-		
-		inc c
-		bit 1, c
-	jr nz, VBLANK.loadColors
-	
+	ldh a, [redraw_screen]
+	ld e, a
 	xor a
 	ldh [redraw_screen], a
 	
+	rr e
+	call c, oamRoutine
+	
+	rr e
+	jr nc, VBLANK.noScroll
+		ld hl, shadow_scroll_y
+		ldi a, [hl]
+		ldh [IO_SCROLL_Y], a
+		ldi a, [hl]
+		ldh [IO_SCROLL_X], a
+		ldi a, [hl]
+		ldh [IO_WINDOW_Y], a
+		ldi a, [hl]
+		ldh [IO_WINDOW_X], a
+	
+	.noScroll:
+	ei
+	rr e
+	jr c, VBLANK.noPalettes
+		ld c, LOW(IO_CRAM_BKG_SELECT)
+		.loadColors:
+			ld b, 0x20/COLORS_PER_LOOP
+			ld a, CRAM_INCREMENT
+			ldh [c], a
+			inc c
+			.copy:
+				REPT COLORS_PER_LOOP*2
+				ldi a, [hl]
+				ldh [c], a
+				ENDR
+				dec b
+			jr nz, VBLANK.copy
+			
+			inc c
+			bit 1, c
+		jr nz, VBLANK.loadColors
+	
+	.noPalette:
 	pop af
 	ldh [IO_WRAM_BANK], a
 	
 	pop hl
+	pop de
 	pop bc
 	pop af
 	add sp, 0x0002
