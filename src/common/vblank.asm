0479b645413d91ceee5c24d9a028f434c0c9906f
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-03-09 00:01:37 -0500

    process graphics tasks

--- src/common/vblank.asm
+++ src/common/vblank.asm
@@ -57,6 +57,44 @@ VBLANK::
 		jr nz, VBLANK.loadColors
 	
 	.noPalettes:
+	rr e
+	jr nc, VBLANK.noGfxTasks
+		ld hl, gfx_task_head
+		.gfx_loop:
+			ld l, [hl]
+			ldi a, [hl]
+			ldh [IO_DMA_SRC_L], a
+			and 0x07
+			ldh [IO_WRAM_BANK], a
+			ldi a, [hl]
+			ldh [IO_DMA_SRC_H], a
+			ldi a, [hl]
+			ld [MBC_ROM_BANK], a
+			ldi a, [hl]
+			ldh [IO_DMA_DST_L], a
+			and 0x01
+			ldh [IO_VRAM_BANK], a
+			ldh a, [IO_LCD_CURRENT_LINE]
+			cpl
+			add 0x9A
+			swap a
+			rrca
+			ld d, a
+			ldi a, [hl]
+			cp d
+				jr nc, .noGfxTasks
+			ldh [IO_DMA_TRIGGER], a
+			ld a, l
+			ld l, gfx_task_head
+			cp LOW(gfx_task_queue + 2 + 6*10)
+			jr c, .savePtr
+				ld a, LOW(gfx_task_tail)
+			.savePtr:
+			ldd [hl], a
+			cp [hl]
+			inc hl
+		jr z, .gfx_loop			
+	.noGfxTasks:
 	pop af
 	ldh [IO_WRAM_BANK], a
 	
